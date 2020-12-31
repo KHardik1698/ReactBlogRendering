776046169e6f2a5760309841716e729e00cf53d9
KHardik1698 <hardik.kundalia@contentstack.com>
2020-12-31 17:42:18 +0530

    Added Blog Markup to render a single blog using fetch api call
    Added Title, Author, Image, Content & Related Links to Blog Markup for rendering

--- src/components/TilesMarkup.jsx
+++ src/components/TilesMarkup.jsx
@@ -7,14 +7,6 @@ class TilesMarkup extends Component {
         <h2>{this.props.blog.title}</h2>
         <h3>{this.props.blog.author}</h3>
         <img src={this.props.blog.imageUrl} alt="" />
-        {/* <p>{this.props.blog.content}</p> */}
-        {/* {this.props.blog.links.map((link) => {
-          return (
-            <div>
-              <p>{link.title}</p>
-            </div>
-          );
-        })} */}
       </div>
     );
   }
--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -1,10 +1,29 @@
 import { Component } from "react";
+import blogsUrl from "../apiCalls/ApiCalls";
+import BlogMarkup from "../components/BlogMarkup";
 
 class BlogPage extends Component {
+  state = {
+    blog: [],
+    status: "",
+  };
+  componentDidMount = (event) => {
+    fetch(blogsUrl + "2rvqpdbpka3n3fhh")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({ blog: data.blogData, status: "Successful" });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   render() {
     return (
       <div>
         <h1>Blog Page Component</h1>
+        <BlogMarkup blog={this.state.blog} status={this.state.status} />
       </div>
     );
   }
