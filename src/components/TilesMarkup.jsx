4b654370c0c7c5cf289573ea635870c5b3dedd0c
KHardik1698 <hardik.kundalia@contentstack.com>
2020-12-31 17:03:13 +0530

    Added Title, Author & Image to Tiles Markup for rendering

--- src/components/TilesMarkup.jsx
+++ src/components/TilesMarkup.jsx
@@ -4,7 +4,17 @@ class TilesMarkup extends Component {
   render() {
     return (
       <div>
-        <p>{this.props.blog.title}</p>
+        <h2>{this.props.blog.title}</h2>
+        <h3>{this.props.blog.author}</h3>
+        <img src={this.props.blog.imageUrl} alt="" />
+        {/* <p>{this.props.blog.content}</p> */}
+        {/* {this.props.blog.links.map((link) => {
+          return (
+            <div>
+              <p>{link.title}</p>
+            </div>
+          );
+        })} */}
       </div>
     );
   }
