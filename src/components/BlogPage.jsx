ae43f8236c12c5c9d0fc650f4fdcb7c0d22267fd
KHardik1698 <hardik.kundalia@contentstack.com>
2020-12-31 14:47:34 +0530

    Blog Page Component Created
    Rendered sample markup

---
+++ src/components/BlogPage.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class BlogPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Blog Page Component</h1>
+      </div>
+    );
+  }
+}
+
+export default BlogPage;
--- src/routers/router.js
+++ src/routers/router.js
@@ -1,6 +1,7 @@
 import { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import BlogTiles from "../components/BlogTiles";
+import BlogPage from "../components/BlogPage";
 
 class Router extends Component {
   render() {
@@ -10,10 +11,7 @@ class Router extends Component {
           <h1>Hello</h1>
           <Switch>
             <Route path="/blogs" exact component={BlogTiles} />
-            <Route path="/blogs/:id" exact>
-              <h1>Single Blog</h1>
-              {/* Single Blog component here */}
-            </Route>
+            <Route path="/blogs/:id" exact component={BlogPage} />
           </Switch>
         </BrowserRouter>
       </div>
