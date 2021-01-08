816aab13bd840a90d784f88c44041bc86e0b0ef6
KHardik1698 <hardik.kundalia@contentstack.com>
2021-01-08 13:45:59 +0530

    Styling Changes in Page Not Found Component
    Updated Image position & size, updated button styling

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -6,11 +6,11 @@ class NotFound extends Component {
   render() {
     return (
       <div className={styles["not-found-container"]}>
-        {/* <img
+        <img
           className={styles["not-found-image"]}
           src="https://image.freepik.com/free-vector/404-error-page-found_41910-364.jpg"
           alt="Page Not Found"
-        /> */}
+        />
         <Link className={styles["back-button"]} to="/blogs">
           <button>Go Back</button>
         </Link>
