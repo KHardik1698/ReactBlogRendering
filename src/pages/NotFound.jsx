80066c3205e2736fc9ba6a0dec2261900008b71c
KHardik1698 <hardik.kundalia@contentstack.com>
2021-01-08 11:47:06 +0530

    Added 404 Not Found Image in Not Found Component
    Created basic styling for Not Found Component

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,10 +1,19 @@
 import { Component } from "react";
+import styles from "../styles/NotFound.module.css";
+import { Link } from "react-router-dom";
 
 class NotFound extends Component {
   render() {
     return (
-      <div>
-        <h1>Page Not Found Component</h1>
+      <div className={styles["not-found-container"]}>
+        {/* <img
+          className={styles["not-found-image"]}
+          src="https://image.freepik.com/free-vector/404-error-page-found_41910-364.jpg"
+          alt="Page Not Found"
+        /> */}
+        <Link className={styles["back-button"]} to="/blogs">
+          <button>Go Back</button>
+        </Link>
       </div>
     );
   }
