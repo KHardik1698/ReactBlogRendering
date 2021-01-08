4ee9ac42d07b09c768774a5fa4e11d29bf11678b
KHardik1698 <hardik.kundalia@contentstack.com>
2021-01-08 11:15:51 +0530

    Created Module Stylesheet for Blog Page
    Added Basic Blog Styling

--- src/components/BlogMarkup.jsx
+++ src/components/BlogMarkup.jsx
@@ -1,26 +1,34 @@
 import { Component } from "react";
 import { Link } from "react-router-dom";
+import styles from "../styles/BlogMarkup.module.css";
 
 class BlogMarkup extends Component {
   render() {
     return (
-      <div>
-        <h2>{this.props.blog.title}</h2>
-        <h3>{this.props.blog.author}</h3>
-        <img src={this.props.blog.imageUrl} alt="Blog" />
-        <p>{this.props.blog.content}</p>
-        {this.props.blog.links.map((link, i) => {
-          return (
-            <div key={`${link.id}${i}`}>
-              <Link
-                to={`/blogs/${link.id}`}
-                onClick={() => this.props.getBlog(link.id)}
-              >
-                <p>{link.title}</p>
-              </Link>
-            </div>
-          );
-        })}
+      <div className={styles["blog-container"]}>
+        <h1 className={styles["blog-title"]}>{this.props.blog.title}</h1>
+        <h1 className={styles["blog-author"]}>{this.props.blog.author}</h1>
+        <img
+          className={styles["blog-image"]}
+          src={this.props.blog.imageUrl}
+          alt="Blog"
+        />
+        <p className={styles["blog-content"]}>{this.props.blog.content}</p>
+        <div className={styles["blog-links"]}>
+          {this.props.blog.links.map((link, i) => {
+            return (
+              <div key={`${link.id}${i}`}>
+                <Link
+                  className={styles["blog-related-link"]}
+                  to={`/blogs/${link.id}`}
+                  onClick={() => this.props.getBlog(link.id)}
+                >
+                  <p>{link.title}</p>
+                </Link>
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   }
