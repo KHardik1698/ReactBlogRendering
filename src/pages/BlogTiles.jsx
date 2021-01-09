20aed491cf60180f92c0981c0c5885856d0df20c
KHardik1698 <hardik.kundalia@contentstack.com>
2021-01-09 23:25:24 +0530

    Styling Changes in Blog Tiles Component
    Added Background Color, Tiles Shadow & Hover Effect

--- src/pages/BlogTiles.jsx
+++ src/pages/BlogTiles.jsx
@@ -2,6 +2,8 @@ import { Component } from "react";
 import { Link } from "react-router-dom";
 import blogsUrl from "../apiCalls/ApiCalls";
 import TilesMarkup from "../components/TilesMarkup";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 import NotFound from "./NotFound";
 import styles from "../styles/BlogTiles.module.css";
 
@@ -29,18 +31,22 @@ class BlogTiles extends Component {
           <h1 className={styles["loading"]}>Loading...</h1>
         ) : this.state.status === "Successful" ? (
           <div>
-            <h1 className={styles["blogs-header"]}>My Blogs</h1>
-            <div className={styles["main-container"]}>
-              {this.state.blogs.map((blog, i) => {
-                return (
-                  <div className={styles["card-container"]} key={blog.id}>
-                    <Link className={styles["card-link"]} to={`/blogs/${blog.id}`}>
-                      <TilesMarkup blog={blog} i={i + 1} />
-                    </Link>
-                  </div>
-                );
-              })}
+            <Header />
+            <div className={styles["body-container"]}>
+              <h1 className={styles["blogs-header"]}>Blogs Collection</h1>
+              <div className={styles["main-container"]}>
+                {this.state.blogs.map((blog, i) => {
+                  return (
+                    <div className={styles["card-container"]} key={blog.id}>
+                      <Link className={styles["card-link"]} to={`/blogs/${blog.id}`}>
+                        <TilesMarkup blog={blog} i={i + 1} />
+                      </Link>
+                    </div>
+                  );
+                })}
+              </div>
             </div>
+            <Footer />
           </div>
         ) : (
           <NotFound />
