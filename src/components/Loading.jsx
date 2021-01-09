f29c107fb5f6797ff9ac9a9a42f14d903225d32b
KHardik1698 <hardik.kundalia@contentstack.com>
2021-01-10 01:42:23 +0530

    Updated Loading Component
    Loading Component Size & Position updated

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -4,7 +4,7 @@ import Loader from "react-loader-spinner";
 function Loading() {
   return (
     <div className={styles["header-container"]}>
-      <Loader type="Oval" color="#00BFFF" height={80} width={80} />
+      <Loader type="Oval" color="#00BFFF" height={60} width={60} />
     </div>
   );
 }
