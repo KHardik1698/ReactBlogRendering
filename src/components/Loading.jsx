3655b0da05df6dd8c6e290b69bfa4bcb13fdaa13
KHardik1698 <hardik.kundalia@contentstack.com>
2021-01-11 01:17:07 +0530

    Added Responsive styling to Loading Component

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -4,7 +4,7 @@ import Loader from "react-loader-spinner";
 function Loading() {
   return (
     <div className={styles["header-container"]}>
-      <Loader type="Oval" color="#00BFFF" height={60} width={60} />
+      <Loader type="Oval" color="#00BFFF" height={"10vw"} width={"10vw"} />
     </div>
   );
 }
