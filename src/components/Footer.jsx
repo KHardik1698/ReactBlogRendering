17e703dd07f2d2f9d925b14022b63676b93dbda5
KHardik1698 <hardik.kundalia@contentstack.com>
2021-01-10 18:16:51 +0530

    Added Responsive styling to Footer Component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,11 +12,11 @@ function Footer() {
     <div className={styles["footer-container"]}>
       <h1 className={styles["copyright"]}>Developed by Hardik &copy; RE/CS</h1>
       <div className={styles["icons-container"]}>
-        <FaFacebookSquare className={styles["icon"]} size="8vh" color="#143cdc" />
-        <FaTwitterSquare className={styles["icon"]} size="8vh" color="#10b9bf" />
-        <FaInstagramSquare className={styles["icon"]} size="8vh" color="#dd29dd" />
-        <FaGithubSquare className={styles["icon"]} size="8vh" color="#00040c" />
-        <FaLinkedin className={styles["icon"]} size="8vh" color="#1f6dc0" />
+        <FaFacebookSquare className={styles["icon"]} size="7vh" color="#143cdc" />
+        <FaTwitterSquare className={styles["icon"]} size="7vh" color="#10b9bf" />
+        <FaInstagramSquare className={styles["icon"]} size="7vh" color="#dd29dd" />
+        <FaGithubSquare className={styles["icon"]} size="7vh" color="#00040c" />
+        <FaLinkedin className={styles["icon"]} size="7vh" color="#1f6dc0" />
       </div>
     </div>
   );
